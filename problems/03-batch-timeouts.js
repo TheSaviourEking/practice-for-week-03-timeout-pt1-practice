b7b05f82381b7b8076162b3cafda476b7016059d
TheSaviourEking <saviour.eking@outlook.com>
2023-09-04 00:53:23 +0100

    add 03-batch-timeouts.js solution

--- problems/03-batch-timeouts.js
+++ problems/03-batch-timeouts.js
@@ -34,8 +34,29 @@ console.log(timeoutObjs); // [ Timeout {...},  Timeout {...}, Timeout {...} ]
 
 function batchTimeouts(callbacks, delays) {
   // Your code here
+  let result = [];
+  
+  for (let i = 0; i < callbacks.length; i++) {
+    result.push(setTimeout(callbacks[i], delays[i]));
+  }
+
+  return result;
 }
 
+const sayHello = () => console.log('hi');
+const sayGoodbye = () => console.log('bye');
+const shout = () => console.log('WHAT?');
+const tasks = [sayHello, sayGoodbye, shout];
+const delays = [500, 200, 900];
+
+const timeoutObjs = batchTimeouts(tasks, delays); 
+// should print: 
+//  'bye' after 200 ms
+//  'hi' after 500 ms
+//  'WHAT?' after 900 ms
+
+// setTimeout(func(), time)
+console.log(timeoutObjs); // [ Timeout {...},  Timeout {...}, Timeout {...} ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = batchTimeouts;
